0df315dbf72f00e1e656bcfd2692179b2b8e8dc9
deesingh13 <deesingh13@WKWIN0801563.global.publicisgroupe.net>
2019-04-03 15:31:54 +0530

    added new files for palindrome check

---
+++ app/controllers/PalindromeResult.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * @author deesingh13
+ *
+ */
+public class PalindromeResult extends Controller {
+
+	public Result check() {
+		String num = request().getQueryString("Number");
+		int number = Integer.parseInt(num);
+		int numberCopy = number;
+		int temp = 0;
+		while(numberCopy%10 != 0) {
+			temp = temp*10+numberCopy%10;
+			numberCopy = numberCopy/10;
+		}
+		if(temp == number)
+			return ok(number + " is a palindrome number.");
+		else
+			return ok(number + " is not a palindrome number.");
+	}
+}
